0653b99df376a53b486297d6f46e3b3886472e10
nicky evers <livelyevers@gmail.com>
2017-03-13 09:32:02 -0700

    updated logic slightly

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -30,8 +30,8 @@ blogView.handleCategoryFilter = function() {
 blogView.handleMainNav = function() {
   $('.main-nav').on('click', '.tab', function() {
     $('.tab-content').hide();
-    var $clickedTab = $(this).attr('data-content');
-    $('#' + $clickedTab).fadeIn(1000); 
+    var clickedTab = $(this).attr('data-content');
+    $('#' + clickedTab).fadeIn(1000); 
   });
   $('.main-nav .tab:first').click();
 };
