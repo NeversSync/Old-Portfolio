427f95cdae891605e68a1494d56a25e0b757cc17
nicky evers <livelyevers@gmail.com>
2017-03-14 15:10:21 -0700

    added public directory and moved files into there

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./portfolio'));
+app.use(express.static('./public'));
 
 app.get('*', function(request, response) {
-  response.sendFile('/index.html', {root: '.'})
+  response.sendFile('/public/index.html', {root: '.'})
 })
-app.get('*', function(request, response) {
-  response.sendFile('/styles/base.css', {root: '.'});
-})
+//   response.sendFile('/styles/base.css', {root: '.'});
+// app.get('*', function(request, response) {
+// })
+// app.get('*', function(request, response) {
+//   response.sendFile('/styles/modules.css', {root: '.'});
+// })
+// app.get('*', function(request, response) {
+//   response.sendFile('/styles/layout.css', {root: '.'});
+// })
 
 app.listen(PORT, function() {
   console.log(`server is up on ${PORT}`)
